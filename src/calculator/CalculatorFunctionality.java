a3bc3059c3820f821a5347c07d107c5782597042
kevin <kevinli37wy@gmail.com>
2019-05-16 23:33:22 -0700

    added ExpressionTokenizer class

--- src/calculator/CalculatorFunctionality.java
+++ src/calculator/CalculatorFunctionality.java
@@ -2,7 +2,87 @@ package calculator;
 
 public class CalculatorFunctionality
 {
-    protected int testNum = 1;
+
 
 }
 
+/**
+ * This class breaks down the expression into tokens: numbers, parentheses and operators
+ */
+class ExpressionTokenizer
+{
+    private String input;
+    // the start of the next token
+    private int start;
+    // one position after the end of the next token
+    private int end;
+
+    public ExpressionTokenizer(String input)
+    {
+        // sets the input to the expression we want to process
+        this.input = input;
+        start = 0;
+        end = 0;
+
+        // gets the first null token, so the next will be the first token if the input
+        // expresion is not empty
+        nextToken();
+    }
+
+    /**
+     * Peeks the next token without moving the start index to the next one (without consuming the
+     * next token)
+     * @return the next token or null if reached the end of the expression
+     */
+    public String peekToken()
+    {
+        if (start >= input.length())
+        {
+            return null;
+        }
+        else
+        {
+            return input.substring(start, end);
+        }
+    }
+
+    /**
+     * Gets the next token and move the start index to the next token
+     * @return
+     */
+    public String nextToken()
+    {
+        // gets the next token
+        String r = peekToken();
+
+        // moves the start index to the next token
+        start = end;
+
+        // if there are no more tokens left, return null (protects the index going out of boundary)
+        if (start >= input.length())
+        {
+            return r;
+        }
+
+        // checks if the first character is a digit
+        if (Character.isDigit(input.charAt(start)))
+        {
+            end = start + 1;
+
+            // if the next character is still a digit, sets the end index one position after the
+            // end of the number
+            while (end < input.length() && Character.isDigit(input.charAt(end)))
+            {
+                end++;
+            }
+
+        }
+        // if the first character is not a digit
+        else
+        {
+            end = start + 1;
+        }
+
+        return r;
+    }
+}
--- src/calculator/CalculatorTester.java
+++ src/calculator/CalculatorTester.java
@@ -6,5 +6,18 @@ public class CalculatorTester
 {
     public static void main(String[] args)
     {
+        ExpressionTokenizer expression = new ExpressionTokenizer("(1+2)*36");
+
+        System.out.println(expression.nextToken());
+        System.out.println(expression.peekToken());
+        System.out.println(expression.nextToken());
+        System.out.println(expression.nextToken());
+        System.out.println(expression.nextToken());
+        System.out.println(expression.nextToken());
+        System.out.println(expression.nextToken());
+        System.out.println(expression.nextToken());
+        System.out.println(expression.nextToken());
+        System.out.println(expression.nextToken());
+        System.out.println(expression.nextToken());
     }
 }
